bc9cc7e542bbc442134559dba7d6a8ce48ae3c92
being-irl <ijbloomf@gmail.com>
2021-06-15 14:51:50 -0700

    First commit

--- prime.py
+++ prime.py
@@ -1,8 +1,8 @@
 ## Program that displays all prime numbers from 1 to 250 and stores the results in results.txt
 ## A prime number is a number divisible only by 1 and itself
 
+## Check if number is prime
 def primeCheck(number):
-
     if number > 1:
         for i in range(2, number):
             if (number % i) == 0:
@@ -10,7 +10,8 @@ def primeCheck(number):
                 break
         else:
             print(f'{number} is prime.')
-        
+
+# Check if numbers in specified range are prime
 def prime(lower,upper):
     parameter = range(lower,upper+1)
     for i in parameter:
