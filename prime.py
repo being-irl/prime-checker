2147339815b8821dcf6f3eea4dbe54092d9a335f
being-irl <ijbloomf@gmail.com>
2021-06-15 16:13:04 -0700

    First commit

--- prime.py
+++ prime.py
@@ -1,22 +1,29 @@
 ## Program that displays all prime numbers from 1 to 250 and stores the results in results.txt
-## A prime number is a number divisible only by 1 and itself
 
-## Check if number is prime
+## Check if a number is prime, return Boolean value
 def primeCheck(number):
-    if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
-                print(f'{number} is NOT prime.')
+    isPrime = False
+    if number > 1 and number % 2 != 0:    # Primes must be greater than 1 and not even
+        for i in range(2, ((number)//2)): # Any number greater than half the prime cannot be its factor
+            if (number % i) == 0:         # Disregard if number is divisible by any value besides 1 and itself
                 break
         else:
-            print(f'{number} is prime.')
+            isPrime = True
+    return isPrime                        # Confirms if value is prime or not, passes Boolean value to prime()
 
-# Check if numbers in specified range are prime
+# Checks if numbers in specified range are prime, then writes primes to results.txt
 def prime(lower,upper):
-    parameter = range(lower,upper+1)
-    for i in parameter:
-        primeCheck(i)
+    parameter = range(lower,upper+1)      # Initialize range of numbers to check
+    primeList = []                        # Initialize empty list for primes
+    primeFile = open("results.txt", "wt") # Initialize empty text file
+    for i in parameter:                   # For every number in range, append to primeList if prime
+        if primeCheck(i) == True:
+            primeList.append(i)
+    for element in primeList:             # Write every item in primeList to text file
+        primeFile.write(str(element) + ", ")
+    primeFile.close()
+    print(f'Primes have been saved to results.txt. They are as follows: ')
+    return primeList
 
-prime(1,250)
 
 
